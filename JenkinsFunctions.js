c967fa24862543459295874ef1999298ea5f069f
hussam <hussam.is@campus.technion.ac.il>
2020-05-04 15:56:03 +0300

    [Hussam 04/05/2020 | 15:56 ] - Getting all jobs data has been successful. To Do: get dates and build state(color) from Build URL.

--- JenkinsFunctions.js
+++ JenkinsFunctions.js
@@ -2,19 +2,56 @@
 //var jenkinsapi = require('jenkins-api');
 //var jenkins = jenkinsapi.init("http://admin:de713ff17ffd4bdfb3fee76eb6718942@jenkins.yoursite.com");
 //connect jenkins user
+var fs = require('fs');
+var allJobs = {}
+var GlobalContainer;
+
+exports.connectJenkinsUser=function(username,password){
+    return require('jenkins')({
+        baseUrl: 'http://' + username + ':' + password + '@localhost:8080',
+        crumbIssuer: true
+    })
+};
+
+exports.getExistedJobsData = function  (jenkinsUser, callback) {
+    new Promise((resolve, reject) => {
+        jenkinsUser.info(function (err, data) {
+            if (err) {
+                reject(err)
+            } else {
+                let jobs={};
+               // console.log(data);
+                data["jobs"].forEach(element => jobs[element.name] = [element.url, element.color]);
+                resolve(jobs)
+            }
+        });
+    }).then(data => {callback(data);}).catch(err => {console.log(err)})
+    };
+// "Hello world 2"
+exports.get_all_builds= function(jenkinsUser, jobName, callback){
+    jenkinsUser.job.get(jobName, function(err, data) {
+    if (err){
+        return console.log(err);
+    }
+    //console.log(data);
+    let jobDic={};
+    data["builds"].forEach(element => jobDic[element.number] = [element.url]); // jobDic = {18: 'Url1', 19:'Url2'}
+    callback(jobDic);
+    });
+};
 
 
-
-
-var jenkins = require('jenkins')({
-    baseUrl: 'http://admin:de713ff17ffd4bdfb3fee76eb6718942@localhost:8080',
-    crumbIssuer: true
+/*
+exports.getJobData = function(jobName, ){
+jenkins.build.get(jobName, 7, function(err, data) { // ("Hello world 1" == job name, 1 == build number)
+    if (err) throw err;
+    console.log('build', data);
 });
+};
+*/
 
 
-// Jenkins User Data: Build Name, Build URL, Color and more
-const allJobs = {}; // {jobName : url ...}
-
+/*
 jenkins.info(function(err, data) {
     if (err) throw err;
     console.log('info', data);
@@ -36,7 +73,7 @@ jenkins.job.build("Hello world 2", function(err, data) {
     console.log('queue item number', data);
 });
 //some functions we may use:
-/*
+
 // delete job
 jenkins.job.destroy('example', function(err) {
     if (err) throw err;
@@ -54,9 +91,7 @@ jenkins.job.exists('example', function(err, exists) {
     if (err) throw err;
 
     console.log('exists', exists);
-});*/
-
-/*
+});
 
 jenkins.build.get("Hello world 2", 7, function(err, data) { // ("Hello world 1" == job name, 1 == build number)
     if (err) throw err;
---
+++ tryTogetData.js
@@ -0,0 +1,8 @@
+
+exports.export_all_jobs= function (allJobsData){
+    NowUseThisData(allJobsData)
+};
+function NowUseThisData(allJobsData){
+    console.log("function works!");
+    console.log(allJobsData);
+}
