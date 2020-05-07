2bdf6d5f7bd738a16ca2e7526d5415a3a500e4e0
aviluz <avil2101@gmail.com>
2020-05-07 03:21:07 +0300

    impl run every 2 weeks

--- cron.js
+++
@@ -1,7 +0,0 @@
-var CronJob = require('cron').CronJob;
-var email = require('./send_build_report');
-var job = new CronJob('*/5 * * * * *', function() {
-    console.log('You will see this message every 5 seconds');
-    email.sendEmailAllBuilds(email.recipients, email.data);
-}, null, true, 'Israel');
-job.start();
--- send_build_report.js
+++ send_build_report.js
@@ -1,83 +1,102 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
 
 function convertStatus(status) {
     switch (status) {
-        case 1: return "Running";
-        case 2: return "Stopped";
-        case 3: return "Success";
-        case 4: return "Failed";
+        case 1:
+            return "Running";
+        case 2:
+            return "Stopped";
+        case 3:
+            return "Success";
+        case 4:
+            return "Failed";
     }
     return "";
 }
 
 function getStatusColor(status) {
     switch (status) {
-        case 1: return "color:black";
-        case 2: return "color:blue";
-        case 3: return "color:green";
-        case 4: return "color:red";
+        case 1:
+            return "color:black";
+        case 2:
+            return "color:blue";
+        case 3:
+            return "color:green";
+        case 4:
+            return "color:red";
     }
     return "";
 }
+
+
 function title() {
-    return "<H1 dir=ltr>The result:</H1><br>";
+    return "<h3 dir=ltr style='text-align: left'>Last 10 result:</h3><br>"+
+        "<img src=\"cid:logo\" style='display: block; width: 40%;  margin-left: auto; margin-right: auto'/><br>";
 }
 
+function title2() {
+    return "<img src=\"cid:logo\" style='display: block; width: 50%;  margin-left: auto; margin-right: auto'/>" +
+        "<h3 dir=ltr style='text-align: left'>Build details : </h3><br>";
+}
+
+
 function jsonToHtml(_data) {
-    const build = JSON.parse(_data)
-    var output = "<table border=\"1\" style=\"width:100%\" dir=ltr>" +
-        "<tr>" +
-        "    <th>Build Id</th>" +
-        "    <th>Result</th>" +
-        "    <th>Status</th>" +
-        "    <th>Date</th>" +
-        "    <th>Start Time</th>" +
-        "    <th>Name</th>" +
-        "</tr>" ;
-    const td = "<td>";
-    output += "<tr>"
-    output += td + build.build_id + "</td>";
-    output += td + build.result + "</td>";
-    output += "<td style=\"" + getStatusColor(build.status) +"\">" + convertStatus(build.status) + "</td>";
-    output += td + build.date + "</td>";
-    output += td + build.start_time + "</td>";
-    output += td + build.build_name + "</td>";
-    output += "</tr>\n";
-    output += "</table>";
-    return output;
+    var style = fs.readFileSync('reportDetails.css', 'utf8');
+    var template = fs.readFileSync('template.html', 'utf8');
+    const build = JSON.parse(_data);
+    var output ="<ul dir=ltr>" +
+        " <li>Build_Id: " + build.build_id + "</li><br>" +
+        " <li>Result</li><br>" +
+        " <li>Status</li><br>" +
+        " <li>Date</li><br>" +
+        " <li>Start Time</li><br>" +
+        " <li>Name</li><br>" +
+        "</ul>";
+    output += build.build_id;
+    output += build.build_name;
+    output += build.result;
+    output += "<style=\"" + getStatusColor(build.status) + "\">" + convertStatus(build.status);
+    output += build.date;
+    output += build.start_time;
+    output += build.build_name;
+    return template.replace('#css#', style).replace('#body#', output)
 }
 
 function jsonListToHtml(_data) {
-    var pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
+    const style = fs.readFileSync('allTests.css', 'utf8');
+    const template = fs.readFileSync('template.html', 'utf8');
+    const pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
     const json_builds = JSON.parse(_data)
         .filter(build => (build.status == 3 || build.status == 4))
-        .sort((build1, build2) => new Date(build2.date.replace(pattern,'$3-$2-$1')) - new Date(build1.date.replace(pattern,'$3-$2-$1')))
+        .sort((build1, build2) => new Date(build2.date.replace(pattern, '$3-$2-$1')) - new Date(build1.date.replace(pattern, '$3-$2-$1')))
         .slice(0, 10)
         .sort((build1, build2) => build2.status - build1.status);
-    var output = "<table border=\"1\" style=\"width:100%\" dir=ltr>" +
+    const th = '<th class="text-left">'
+    var output = '<table border="1" style="width:100%" dir=ltr class="table-fill">' +
         "<tr>" +
-        "    <th>Build Id</th>" +
-        "    <th>Result</th>" +
-        "    <th>Status</th>" +
-        "    <th>Date</th>" +
-        "    <th>Start Time</th>" +
-        "</tr>" ;
+        th + "Build Id</th>" +
+        th + "Result</th>" +
+        th + "Status</th>" +
+        th + "Date</th>" +
+        th + "Start Time</th>" +
+        "</tr>";
 
     json_builds.forEach(build => {
-        const td = "<td>";
+        const td = '<td class="text-left">';
         output += "<tr>"
         output += td + build.build_id + "</td>";
         output += td + build.result + "</td>";
-        output += "<td style=\"" + getStatusColor(build.status) +"\">" + convertStatus(build.status) + "</td>";
+        output += "<td style=\"" + getStatusColor(build.status) + "\">" + convertStatus(build.status) + "</td>";
         output += td + build.date + "</td>";
         output += td + build.start_time + "</td>";
         output += "</tr>\n";
     })
     output += "</table>";
-    return output;
+    return template.replace('#css#', style).replace('#body#', output);
 }
 
-exports.sendEmailAllBuilds = function(_to, _data) {
+exports.sendEmailAllBuilds = function (_to, _data) {
     console.log("sendEmailAllBuilds()");
     const _user = 'OPCloudSoftware@gmail.com';
     const transporter = nodemailer.createTransport({
@@ -88,15 +107,21 @@ exports.sendEmailAllBuilds = function(_to, _data) {
         }
     });
 
+    let title1 = title;
     const mailOptions = {
         from: _user,
         to: _to.toString(),
-        subject: 'Build report from Jenkins ' +  Date(),
+        subject: 'Build report from Jenkins',
         text: _data,
-        html: title() + jsonListToHtml(_data)
+        html: title1() + jsonListToHtml(_data),
+        attachments: [{
+            filename: 'image.png',
+            path : "image.png",
+            cid: 'logo' //same cid value as in the html img src
+        }]
     };
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
         } else {
@@ -104,7 +129,7 @@ exports.sendEmailAllBuilds = function(_to, _data) {
         }
     });
 }
-exports.sendBuildDetails = function(_to, _data) {
+exports.sendBuildDetails = function (_to, _data) {
     console.log("sendBuildDetails()");
     const _user = 'OPCloudSoftware@gmail.com';
     const transporter = nodemailer.createTransport({
@@ -114,16 +139,21 @@ exports.sendBuildDetails = function(_to, _data) {
             pass: 'OPCloudSoftwareIntegrityManagementSystem'
         }
     });
-
+    let title3 = title2;
     const mailOptions = {
         from: _user,
         to: _to.toString(),
-        subject: 'Build details report from Jenkins ' +  Date(),
+        subject: 'Build details report from Jenkins ' + new Date().toLocaleString(),
         text: _data,
-        html: title() + jsonToHtml(_data)
+        html: title3() + jsonToHtml(_data),
+        attachments: [{
+            filename: 'image.png',
+            path : "image.png",
+            cid: 'logo' //same cid value as in the html img src
+        }]
     };
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
         } else {
@@ -131,103 +161,3 @@ exports.sendBuildDetails = function(_to, _data) {
         }
     });
 }
-//--------------------------- Example---------------------------------------
-exports. data = '[\n' +
-    '\t{\n' +
-    '\t  "build_id": 23,\n' +
-    '\t  "status": 1,\n' +
-    '\t  "result": "28/50",\n' +
-    '\t  "date": "28/03/2020",\n' +
-    '\t  "start_time": "10:45:44"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 24,\n' +
-    '\t  "status": 4,\n' +
-    '\t  "result": "12/40",\n' +
-    '\t  "date": "27/02/2020",\n' +
-    '\t  "start_time": "21:29:12"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 29,\n' +
-    '\t  "status": 3,\n' +
-    '\t  "result": "34/50",\n' +
-    '\t  "date": "01/04/2020",\n' +
-    '\t  "start_time": "22:55:43"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 2,\n' +
-    '\t  "status": 2,\n' +
-    '\t  "result": "19/30",\n' +
-    '\t  "date": "02/04/2020",\n' +
-    '\t  "start_time": "23:23:57"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 88,\n' +
-    '\t  "status": 3,\n' +
-    '\t  "result": "15/47",\n' +
-    '\t  "date": "03/04/2020",\n' +
-    '\t  "start_time": "11:30:39"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 56,\n' +
-    '\t  "status": 4,\n' +
-    '\t  "result": "40/50",\n' +
-    '\t  "date": "04/04/2020",\n' +
-    '\t  "start_time": "01:57:33"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 43,\n' +
-    '\t  "status": 4,\n' +
-    '\t  "result": "29/33",\n' +
-    '\t  "date": "04/04/2020",\n' +
-    '\t  "start_time": "12:01:09"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 12,\n' +
-    '\t  "status": 3,\n' +
-    '\t  "result": "32/32",\n' +
-    '\t  "date": "04/04/2020",\n' +
-    '\t  "start_time": "18:19:12"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 76,\n' +
-    '\t  "status": 4,\n' +
-    '\t  "result": "10/50",\n' +
-    '\t  "date": "05/08/2020",\n' +
-    '\t  "start_time": "10:23:40"\n' +
-    '  },\n' +
-    '  {\n' +
-    '\t  "build_id": 23,\n' +
-    '\t  "status": 4,\n' +
-    '\t  "result": "28/37",\n' +
-    '\t  "date": "20/04/2020",\n' +
-    '\t  "start_time": "19:56:10"\n' +
-    '  }\n' +
-    ']'
-exports.build = "{\n" +
-    "  \"build_id\": 23,\n" +
-    "  \"status\": 1,\n" +
-    "  \"result\": \"28/50\",\n" +
-    "  \"date\": \"28/03/2020\",\n" +
-    "  \"start_time\": \"10:45:44\",\n" +
-    "  \"build_name\" : \"Test 23\",\n" +
-    "  \"tests\": [\n" +
-    "    {\n" +
-    "      \"test_id\": 1,\n" +
-    "      \"test_case\": \"bla\",\n" +
-    "      \"test_title\": \"o1-o2\"\n" +
-    "    },\n" +
-    "    {\n" +
-    "      \"test_id\": 2,\n" +
-    "      \"test_case\": \"alb\",\n" +
-    "      \"test_title\": \"o3-o42\"\n" +
-    "\n" +
-    "    }\n" +
-    "  ]\n" +
-    "}";
-
-exports.recipients = ['avil2101@gmail.com'];
-
-//exports.sendEmailAllBuilds(recipients, data);
-//exports.sendBuildDetails(recipients, build);
-
